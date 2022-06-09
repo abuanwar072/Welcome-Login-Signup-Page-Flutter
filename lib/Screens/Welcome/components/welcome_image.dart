00e0001e29e61b2705d8444f659079a38cabd50e
Abu Anwar <abuanwar072@gmail.com>
2022-06-09 08:46:31 +0600

    welcome screen refector

---
+++ lib/Screens/Welcome/components/welcome_image.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../../constants.dart';
+
+class WelcomeImage extends StatelessWidget {
+  const WelcomeImage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          "WELCOME TO EDU",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        SizedBox(height: defaultPadding * 2),
+        Row(
+          children: [
+            Spacer(),
+            Expanded(
+              flex: 8,
+              child: SvgPicture.asset(
+                "assets/icons/chat.svg",
+              ),
+            ),
+            Spacer(),
+          ],
+        ),
+        SizedBox(height: defaultPadding * 2),
+      ],
+    );
+  }
+}
--- lib/Screens/Welcome/welcome_screen.dart
+++ lib/Screens/Welcome/welcome_screen.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
-import '../../Screens/Login/login_screen.dart';
-import '../../Screens/Signup/signup_screen.dart';
 import '../../components/background.dart';
-import '../../constants.dart';
+import 'components/login_signup_btn.dart';
+import 'components/welcome_image.dart';
 
 class WelcomeScreen extends StatelessWidget {
   @override
@@ -15,70 +13,13 @@ class WelcomeScreen extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Text(
-                "WELCOME TO EDU",
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: defaultPadding * 2),
+              WelcomeImage(),
               Row(
                 children: [
                   Spacer(),
                   Expanded(
                     flex: 8,
-                    child: SvgPicture.asset(
-                      "assets/icons/chat.svg",
-                    ),
-                  ),
-                  Spacer(),
-                ],
-              ),
-              SizedBox(height: defaultPadding * 2),
-              Row(
-                children: [
-                  Spacer(),
-                  Expanded(
-                    flex: 8,
-                    child: Column(
-                      children: [
-                        Hero(
-                          tag: "login_btn",
-                          child: ElevatedButton(
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) {
-                                    return LoginScreen();
-                                  },
-                                ),
-                              );
-                            },
-                            child: Text(
-                              "Login".toUpperCase(),
-                            ),
-                          ),
-                        ),
-                        const SizedBox(height: 16),
-                        ElevatedButton(
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) {
-                                  return SignUpScreen();
-                                },
-                              ),
-                            );
-                          },
-                          style: ElevatedButton.styleFrom(
-                              primary: kPrimaryLightColor, elevation: 0),
-                          child: Text(
-                            "Sign Up".toUpperCase(),
-                            style: TextStyle(color: Colors.black),
-                          ),
-                        ),
-                      ],
-                    ),
+                    child: LoginAndSignupBtn(),
                   ),
                   Spacer(),
                 ],
